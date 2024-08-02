50cbfc1dfd3559ed0e570b606ec6d194765091fc
ren <ren_pers@yahoo.com>
2024-08-03 00:11:02 +0200

    use redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { BrowserRouter, Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
-import {useEffect} from "react";
 
-const RedirectHandler = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const currentPath = location.pathname;
-        // Перенаправляем на '/' если путь не совпадает с другими маршрутами
-        if (currentPath !== '/' && currentPath !== '/video/:id' && currentPath !== '/channel/:id' && currentPath !== '/search/:searchTerm') {
-            navigate('/');
-        }
-    }, [location, navigate]);
-
-    return null;
-};
 
 const App = () => (
     <BrowserRouter>
         <Box sx={{ backgroundColor: '#000' }}>
             <Navbar />
             <Routes>
-                <Route exact path='/' element={<Feed />} />
+                <Route path='/' element={<Feed />} />
                 <Route path='/video/:id' element={<VideoDetail />} />
                 <Route path='/channel/:id' element={<ChannelDetail />} />
                 <Route path='/search/:searchTerm' element={<SearchFeed />} />
             </Routes>
-            <RedirectHandler />
         </Box>
     </BrowserRouter>
 );

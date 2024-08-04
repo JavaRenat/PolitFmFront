156c0da9a942a65dc3717d35b652aa147bece07a
ren <ren_pers@yahoo.com>
2024-08-04 02:07:31 +0200

    switch to backend

--- src/utils/fetchFromAPI.jsx
+++ src/utils/fetchFromAPI.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = '/backend';
+export const BASE_URL = `${window.location.origin}/backend`;
 // export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {

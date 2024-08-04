a2011e4a8935c7dfe532ede37ad38c2aa611ff41
ren <ren_pers@yahoo.com>
2024-08-05 00:40:22 +0200

    switch to backend

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,5 @@ export default defineConfig({
   build: {
     outDir: './docs'
   },
-  base: './',
-  server: {
-    proxy: {
-      '/backend': {
-        target: 'http://94.156.35.132:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/backend/, ''),
-        secure: false
-      }
-    }
-  }
+  base: './'
 })
